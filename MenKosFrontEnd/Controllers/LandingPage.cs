8bc4f4e46a9fc1b56fd29f80fd8ea40686e4f1af
Arief <arief.riski11@gmail.com>
2022-11-30 09:59:53 +0700

    add landing page template

---
+++ MenKosFrontEnd/Controllers/LandingPage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MenKosFrontEnd.Controllers
+{
+    public class LandingPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RentDetail()
+        {
+            return View();
+        }
+    }
+}
