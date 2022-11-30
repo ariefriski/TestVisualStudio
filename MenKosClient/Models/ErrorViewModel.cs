dcc87ec5e47d4c294ad20b1d685463674b512ef9
Arief <arief.riski11@gmail.com>
2022-11-30 12:07:30 +0700

    first commit

---
+++ MenKosClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenKosClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
