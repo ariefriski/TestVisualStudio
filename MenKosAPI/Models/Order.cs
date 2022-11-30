929616fda78dfbf58d7284cece11c59de72f22b0
panji <prawiro.panji@gmail.com>
2022-11-30 15:44:19 +0700

    add Models
    create models files and connection string

---
+++ MenKosAPI/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace MenKosAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateOnly EntryDate { get; set; }
+        public DateOnly OutDate { get; set; }
+        public Occupant OccupantId { get; set; }
+    }
+}
---
+++ MenKosAPI/Models/RoomPicture.cs
@@ -0,0 +1,9 @@
+﻿namespace MenKosAPI.Models
+{
+    public class RoomPicture
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string RoomType { get; set; }
+    }
+}
