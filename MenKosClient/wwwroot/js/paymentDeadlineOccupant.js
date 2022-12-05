a3dad703ddc90fcadbe6fed667726cea70ab54d0
prawiropanji <prawiro.panji@gmail.com>
2022-12-05 23:49:04 +0700

    create razor view page for payement deadline per occupant

--- MenKosClient/wwwroot/js/paymentDeadlineOccupant.js
+++ MenKosClient/wwwroot/js/paymentDeadlineOccupant.js
@@ -1,3 +1,22 @@
-﻿$.ajax({
-    url: 'https://localhost:7095/api/transaction/paymentdeadline/'
+﻿const occupantId = document.getElementById('occupantIdTitle').dataset.occupantid
+
+const smallEl = document.getElementById('infoMessage')
+
+$.ajax({
+    url: `https://localhost:7095/api/transaction/paymentdeadline/${occupantId}`,
+    type: 'GET',
+    contentType: 'application/json',
+    success: res => {
+        console.log(res)
+
+        const outDate = new Date(res.Data.Payment.Order.OutDate)
+
+        const currentTime = new Date()
+        currentTime.setHours(0, 0, 0, 0)
+        const currentDate = new Date(currentTime)
+
+        const remainingDay = outDate.getDate() - currentDate.getDate()
+
+        smallEl.textContent = `masa sewa kosan tersisa ${remainingDay} hari lagi!`
+    }
 })
