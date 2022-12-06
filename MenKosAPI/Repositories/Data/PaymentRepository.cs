d1a3be8bbd9fec9f60be08c9ad476ab068b19961
prawiropanji <prawiro.panji@gmail.com>
2022-12-06 16:51:52 +0700

    Update "UpdateStatusPayment" payment repository to update the paymentid FK in room table

--- MenKosAPI/Repositories/Data/PaymentRepository.cs
+++ MenKosAPI/Repositories/Data/PaymentRepository.cs
@@ -6,23 +6,52 @@ namespace MenKosAPI.Repositories.Data
     public class PaymentRepository : GeneralRepositories<Payment>
     {
         private MyContext _context;
-        public PaymentRepository(MyContext myContext) : base(myContext)
+        private OrderRepository _orderRepository;
+        private RoomRepository _roomRepository;
+        public PaymentRepository(MyContext myContext, OrderRepository orderRepository, RoomRepository roomRepository) : base(myContext)
         {
             _context = myContext;
+            _orderRepository = orderRepository;
+            _roomRepository = roomRepository;
         }
 
         public int UpdateStatusPayment(int paymentId)
         {
             var payment = this.Get(paymentId);
             payment.Status = true;
-            int result = this.Update(payment);
+            int resultUpdatePayment = this.Update(payment);
 
-            if (result > 0)
+            if (resultUpdatePayment <= 0)
             {
-                return 1; // update berhasil
+                return 2; // update gagal
             }
 
-            return 2; // update gagal
+
+            var orderId = payment.OrderId;
+            
+            if(orderId == null)
+            {
+                return 2;
+            }
+
+            var order = _orderRepository.Get((int)orderId);
+          
+
+            var roomId = order.RoomId;
+
+            var room = _roomRepository.Get(roomId);
+
+            room.PaymentId = payment.Id;
+            room.Status = true;
+            int resultUpdateRoom = _roomRepository.Update(room);
+            if(resultUpdateRoom <= 0)
+            {
+                return 2; //update gagal
+            }
+
+
+
+            return 1; // update berhasil
 
 
         }
